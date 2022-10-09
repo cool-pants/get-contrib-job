7a586abdeac6edbd9afb25db089c116678c63e09
cool-pants <muban.sg@gmail.com>
2022-10-09 16:17:56 +0530

    [config]: Add json endpoint

--- index.ts
+++ index.ts
@@ -13,25 +13,28 @@ const getHTML = (list: Contributor[]): string => {
     )).join(" ")+"</div>"
 }
 
-app.get('/contributors/:owner/:repo', async (req:any,res:any) => {
+app.get('/contributors/:owner/:repo', async (req,res:any) => {
     const url:string = ["https://api.github.com/repos",req.params.owner,req.params.repo,"contributors"].join("/")
     try{
-    const {data, status} = await axios.get<Contributor[]>(
-        url,
-        {
-            headers: {
-                Accept: 'application/json',
+        const {data, status} = await axios.get<Contributor[]>(
+            url,
+            {
+                headers: {
+                    Accept: 'application/json',
+                },
             },
-        },
-    );
+        );
 
-    if (!status)
-    {
-        res.send("Bad request!")
-        return
-    }               
-    
-    res.send(getHTML(data));
+        if (!status)
+        {
+            res.send("Bad request!")
+            return
+        }               
+        if("json" in req.query){
+            return data.map(i=>({user:i.login, github:i.html_url, avatar:i.avatar_url}))
+        }
+        res.send(getHTML(data.map(i=>({login:i.login,avatar_url:"https://images.weserv.nl/?url="+i.avatar_url+"&h=300&w=300&fit=cover&mask=circle&maxage=7d", html_url:i.html_url}))));
+
     } catch (error) {
         if (axios.isAxiosError(error)) {
             res.send(error.message);
